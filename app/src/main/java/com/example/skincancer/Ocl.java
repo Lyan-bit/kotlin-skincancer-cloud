1481c1de3afd58eb0057eb4dfbc436ba3a13b8bc
Lyan-bit <72354386+Lyan-bit@users.noreply.github.com>
2023-06-11 21:30:03 +0100

    Update Ocl.java

--- app/src/main/java/com/example/skincancer/Ocl.java
+++ app/src/main/java/com/example/skincancer/Ocl.java
@@ -11,6 +11,7 @@ package com.example.skincancer; // Place in the relevant package directory of yo
 * *****************************/
 /* OCL library for Java version 8+ */
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Ocl {
